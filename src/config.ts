7be044832f002e18357e4145a14b34ed74885099
DKavaliou-by <dkavaliou.by@gmail.com>
2022-02-08 17:35:48 +0300

    cleanup

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,8 @@ export const CONFIG = {
   MAPBOX_TOKEN:
     'pk.eyJ1IjoiZGFuaWwta2F2YWxpb3UiLCJhIjoiY2t3ZW5pODFqMDZmZDJwbWp6YjA0ODc2MSJ9.Q2Q-M9S3Ec7xUYekKExvpA',
   MAP_STYLE: 'mapbox://styles/mapbox/light-v10',
+  // MAP_STYLE: 'mapbox://styles/mapbox/satellite-v9',
+  // MAP_STYLE: 'mapbox://styles/mapbox/dark-v10',
+  // MAP_STYLE: 'mapbox://styles/mapbox/streets-v11',
+  // MAP_STYLE: 'mapbox://styles/mapbox/outdoors-v11',
 };
