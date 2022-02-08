80f0e8971aad0abf1510206b1688bbb330c73f28
DKavaliou-by <dkavaliou.by@gmail.com>
2022-02-08 16:35:28 +0300

    Add map, mocked phapi and markers

--- index.ts
+++ index.ts
@@ -1,7 +1,59 @@
 // Import stylesheets
 import './style.css';
-import { CONFIG } from './config';
+import { CONFIG } from './src/config';
+import { phApiMock } from './src/mocked-data/ph-api';
+
+import mapboxgl from 'mapbox-gl';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+mapboxgl.accessToken = CONFIG.MAPBOX_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [phApiMock.coordinates.longitude, phApiMock.coordinates.latitude],
+  zoom: 5,
+});
+
+const geoData = phApiMock.items.map((item, index) => {
+  return {
+    type: 'Feature',
+    properties: {
+      value: item.price,
+      id: index,
+      propertyId: item.id,
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [
+        +item.coordinates.longitude.toFixed(5),
+        +item.coordinates.latitude.toFixed(4),
+      ],
+    },
+  };
+});
+
+const getCircleLayerConfig = (color = 'transperent'): mapboxgl.AnyLayer => ({
+  id: 'properties',
+  type: 'circle',
+  source: 'properties',
+  paint: {
+    'circle-color': color,
+    'circle-radius': 10,
+    'circle-stroke-width': 1,
+    'circle-stroke-color': color,
+  },
+});
+
+map.on('load', () => {
+  map.addSource('properties', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: [...geoData],
+    },
+  });
+
+  map.addLayer(getCircleLayerConfig('#11874D'));
+  // map.addLayer(getCircleLayerConfig());
+});
